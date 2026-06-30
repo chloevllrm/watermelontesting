4f4effb923d0321317d664fecc738cfa61afd36b
chloevllrm <76785646+chloevllrm@users.noreply.github.com>
2026-06-30 11:36:15 -0700

    Delete vars directory

--- vars/executeFunctionalTest.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map options) {
-    echo "Running Test Pack..."
-    echo "Node: ${options.node}"
-    echo "Credential: ${options.credentialId}"
-    echo "Test Pack ID: ${options.TEST_PACK_ID}"
-
-    echo "Test execution simulated ✅"
-}
